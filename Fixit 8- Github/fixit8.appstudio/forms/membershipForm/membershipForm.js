ce2e1dfcbd0897bd21c58d868148a77a956435fe
daniellespell21 <71269685+daniellespell21@users.noreply.github.com>
2020-10-01 12:01:16 -0500

    Add files via upload

---
+++ Fixit 8- Github/fixit8.appstudio/forms/membershipForm/membershipForm.js
@@ -0,0 +1,11 @@
+
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+Input1.onclick=function(){
+  let name=prompt("Enter your first name")
+  let memberName=members + ${name}
+  if (name==true):
+    alert("You are a member")
+  if (name== false)
+    alert("your are not a member.")
+}
